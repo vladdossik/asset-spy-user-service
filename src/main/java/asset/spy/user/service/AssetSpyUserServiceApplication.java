949e099c413c0be96d10d90352490928ad5b4ed8
Anastasiia <nastasiia227@gmail.com>
2025-02-20 13:52:25 +0300

    feat(STOCK-71): Created SpringBoot application skeleton

---
+++ src/main/java/asset/spy/user/service/AssetSpyUserServiceApplication.java
@@ -0,0 +1,14 @@
+package asset.spy.user.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "asset.spy")
+public class AssetSpyUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssetSpyUserServiceApplication.class, args);
+    }
+}
