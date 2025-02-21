d6bf61083351a66845eaf2d03d6477274f3d7fef
Anastasiia <nastasiia227@gmail.com>
2025-02-21 13:07:27 +0300

    removed commented security dependency, annotation componentScan

--- src/main/java/asset/spy/user/service/AssetSpyUserServiceApplication.java
+++ src/main/java/asset/spy/user/service/AssetSpyUserServiceApplication.java
@@ -2,10 +2,8 @@ package asset.spy.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "asset.spy")
 public class AssetSpyUserServiceApplication {
 
     public static void main(String[] args) {
