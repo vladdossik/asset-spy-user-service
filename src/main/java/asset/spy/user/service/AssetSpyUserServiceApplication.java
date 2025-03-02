c9414f82bea59d23405358c5bd0428d2074eccc4
Anastasiia <nastasiia227@gmail.com>
2025-03-02 14:32:50 +0300

    comments fixed

--- src/main/java/asset/spy/user/service/AssetSpyUserServiceApplication.java
+++ src/main/java/asset/spy/user/service/AssetSpyUserServiceApplication.java
@@ -2,9 +2,7 @@ package asset.spy.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
-@EnableSpringDataWebSupport
 @SpringBootApplication
 public class AssetSpyUserServiceApplication {
 
